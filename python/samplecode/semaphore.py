aebc891ac34f4654fb043f2fb6ac78e68f7f1bdf
mitalich24a <mitalic209@gmail.com>
2026-07-21 10:32:27 +0530

    Update semaphore.py

--- python/samplecode/semaphore.py
+++ python/samplecode/semaphore.py
@@ -1,1 +1,20 @@
-s
+from threading import Semaphore
+
+class Foo:
+
+    def __init__(self):
+        self.s1 = Semaphore(0)
+        self.s2 = Semaphore(0)
+
+    def first(self, printFirst):
+        printFirst()
+        self.s1.release()
+
+    def second(self, printSecond):
+        self.s1.acquire()
+        printSecond()
+        self.s2.release()
+
+    def third(self, printThird):
+        self.s2.acquire()
+        printThird()

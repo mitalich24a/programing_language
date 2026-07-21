235d22c9fd57b22636b4c5b5355caf738482e231
mitalich24a <mitalic209@gmail.com>
2026-07-21 10:33:01 +0530

    Update lock.py

--- python/samplecode/lock.py
+++ python/samplecode/lock.py
@@ -1,1 +1,23 @@
-l
+from threading import Lock
+
+class Foo:
+
+    def __init__(self):
+        self.lock2 = Lock()
+        self.lock3 = Lock()
+
+        self.lock2.acquire()
+        self.lock3.acquire()
+
+    def first(self, printFirst):
+        printFirst()
+        self.lock2.release()
+
+    def second(self, printSecond):
+        with self.lock2:
+            printSecond()
+            self.lock3.release()
+
+    def third(self, printThird):
+        with self.lock3:
+            printThird()
